406ba2c7d315240b20f1d9c1aae30995105ded62
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-15 16:33:34 +0600

    added 3 types of struct literals writing method

--- class_23/struct_literal/struct_literal.go
+++ class_23/struct_literal/struct_literal.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main(){
-	subscriber := magazine.Subscriber{
-		Name: "mahmudul Hasan",
-		Rate: 3.00,
-		Active: true,
-	}
+	//here i have wrote 3 types of struct literals writing method
+	subscriber := magazine.Subscriber{Name: "mahmudul Hasan", Rate: 3.00, Active: true}
+
+	// subscriber := magazine.Subscriber{
+	// 	Name: "mahmudul Hasan",
+	// 	Rate: 3.00,
+	// 	Active: true,
+	// }
+
+	// subscriber := magazine.Subscriber{
+	// 	Name: "mahmudul Hasan",
+	// 	Rate: 3.00,
+	// 	Active: true}
+
 	fmt.Println(subscriber.Active)
 	fmt.Println(subscriber.Name)
 }
