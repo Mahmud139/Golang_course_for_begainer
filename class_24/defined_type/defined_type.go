df103184ee746428e1e5e73744cf9531cfc57d71
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-17 22:10:29 +0600

    defined type

--- class_24/defined_type/defined_type.go
+++ class_24/defined_type/defined_type.go
@@ -18,4 +18,14 @@ func main(){
 	fmt.Printf("%T\n",busFuel)
 	fmt.Println(carFuel, busFuel)
 
+	//Use shorthand variable declaration
+	// carFuel := Gallons(10.0)
+	// busFuel := Liters(240.0)
+
+	// carFuel = Liters(10.1)
+	// busFuel = Gallons(240.2)
+	/*If you have a variable that uses a defined type, you cannot assign a value of a
+	different defined type to it, even if the other type has the same underlying type.
+	This helps protect developers from confusing the two types.*/
+
 }
