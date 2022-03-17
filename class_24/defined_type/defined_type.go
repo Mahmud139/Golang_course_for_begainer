4454eb64fe40c02440d305a18b16103c4ebf4be2
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-17 22:06:24 +0600

    defined type with underlying basic type

--- class_24/defined_type/defined_type.go
+++ class_24/defined_type/defined_type.go
@@ -8,9 +8,14 @@ type Gallons float64
 func main(){
 	var carFuel Gallons
 	var busFuel Liters
-	carFuel = 12.12
-	busFuel = 34.23
+	// carFuel = 12.12
+	// busFuel = 34.23
+	// fmt.Printf("%T\n",carFuel)
+	// fmt.Printf("%T",busFuel)
+	carFuel = Gallons(10.0)
+	busFuel = Liters(240.0)
 	fmt.Printf("%T\n",carFuel)
-	fmt.Printf("%T",busFuel)
+	fmt.Printf("%T\n",busFuel)
+	fmt.Println(carFuel, busFuel)
 
 }
