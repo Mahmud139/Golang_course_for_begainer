0bf8613afe8d1dc9fc155cc49a47ac1598f9a50a
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-19 10:18:06 +0600

    methods

--- class_24/methods/methods.go
+++ class_24/methods/methods.go
@@ -2,20 +2,39 @@ package main
 
 import "fmt"
 
-type Litters float64
-type Mililitter float64
+type Liters float64
+type Mililiter float64
 type Gallons float64
 
 func main(){
 
 }
 
-func ToGallons(l Litters) Gallons {
+// func ToGallons(l Litters) Gallons {
+// 	return Gallons(l * 0.264)
+// }
+
+// func ToGallons(m Mililitter) Gallons {
+// 	return Gallons(m * 0.000264)
+// }
+//we can’t have two ToGallons functions in the same package!
+
+func LiterToGallon(l Liters) Gallons {
 	return Gallons(l * 0.264)
 }
 
-func ToGallons(m Mililitter) Gallons {
+func MililiterToGallon(m Mililiter) Gallons {
 	return Gallons(m * 0.000264)
 }
 
-//we can’t have two ToGallons functions in the same package!
+func GallonsToLiter(g Gallons) Liters {
+	return Liters(g * 3.785)
+}
+
+func GallonsToMililiters(g Gallons) Mililiter {
+	return Mililiter(g * 3785.41)
+}
+
+/*But those names would be a pain to write out all the time, and as we start adding
+functions to convert between the other types, it becomes clear this isn’t
+sustainable.*/
