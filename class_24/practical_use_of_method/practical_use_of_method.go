258390882af2578fd75a8bce4b9d499471ceb99e
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-23 20:24:48 +0600

    practical use of methods

--- class_24/practical_use_of_method/practical_use_of_method.go
+++ class_24/practical_use_of_method/practical_use_of_method.go
@@ -1,15 +1,20 @@
 package main
 
+import "fmt"
+
 type Litters float64
 type Mililiter float64
 type Gallons float64
 
-func main(){
-
+func main() {
+	soda := Litters(2)
+	fmt.Printf("%0.2f litters equals %0.3f gallons.\n",soda, soda.ToGallons())
+	water := Mililiter(500)
+	fmt.Printf("%0.2f Mililiter equals %0.3f gallons.\n",water, water.ToGallons())
 }
 
 //unlike functions, method names don’t have to be unique, as long as they’re defined on different types.
-func (l Litters)ToGallons() Gallons {
+func (l Litters) ToGallons() Gallons {
 	return Gallons(l * 0.264)
 }
 
