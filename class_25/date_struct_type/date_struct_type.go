fa8b1a89f4e5a41b01a3bb7450c9d0bc6bfcb7f0
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-24 20:00:54 +0600

    setter method

--- class_25/date_struct_type/date_struct_type.go
+++ class_25/date_struct_type/date_struct_type.go
@@ -11,4 +11,11 @@ type Date struct {
 func main() {
 	date := Date{Year: 2019, Month: 12, Day: 23}
 	fmt.Println(date)
+
+	date = Date{Year: 2019, Month: 14, Day: 50}
+	fmt.Println(date)
+	date = Date{Year: 0, Month: 0, Day: -2}
+	fmt.Println(date)
+	date = Date{Year: -999, Month: -1, Day: 0}
+	fmt.Println(date)
 }
