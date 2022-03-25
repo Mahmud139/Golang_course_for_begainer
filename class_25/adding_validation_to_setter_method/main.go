08ea2a9742c9f7df56c2d8106b79bd153498a9ed
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-25 19:50:52 +0600

    adding validation for setter methods

---
+++ class_25/adding_validation_to_setter_method/main.go
@@ -0,0 +1,32 @@
+//adding_validation_to_setter_method
+package main
+
+import (
+	"fmt"
+	"errors"
+	"log"
+)
+
+type Date struct {
+	Year int
+	Month int
+	Day int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.Year = year 
+	return nil
+}
+
+func main(){
+	date := Date{}
+	//err := date.SetYear(0)
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date.Year)
+}
