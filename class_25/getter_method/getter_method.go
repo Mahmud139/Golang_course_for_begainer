e01f156f02b1db074d64f967e8ff27a65f1de4fe
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-27 19:38:24 +0600

    getter method

---
+++ class_25/getter_method/getter_method.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"golang_tutorial/class_25/calendar"
+	"log"
+)
+
+func main(){
+	date := calendar.Date{}
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(6)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(6)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*Unexported variables, struct fields, functions, and methods can still be
+	accessed by exported functions and methods in the same package.*/
+	fmt.Println(date)
+
+	/*if we try to update main.go to print an
+individual Date field, we won’t be able to access it!*/
+	//fmt.Println(date.year)
+	fmt.Println(date.Year())
+	fmt.Println(date.Month())
+	fmt.Println(date.Day())
+}
