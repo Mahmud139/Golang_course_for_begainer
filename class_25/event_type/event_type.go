510a302e41da6477790950baa00b8f37666de2de
mahmud139 <mahmudulhasan139139@gmail.com>
2022-03-29 20:19:09 +0600

    exported methods get promoted

--- class_25/event_type/event_type.go
+++ class_25/event_type/event_type.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"golang_tutorial/class_25/calendar"
+	"log"
 )
 
 func main(){
 	event := calendar.Event{}
-	fmt.Println(event.Year())
 	//unexported fields don't get promoted to outer struct type
 	// event.month = 5
 	// event.Date.year = 10
+	err := event.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = event.SetMonth(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = event.SetDay(21)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(event.Year())
+	fmt.Println(event.Month())
+	fmt.Println(event.Day())
+	
 }
