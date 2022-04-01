80c2569d97a8dcb308c2ae2943cb9f2cb887e980
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-01 19:47:40 +0600

    defining a type that satisfies an interface

--- class_26/defining_interface/interface2.go
+++ class_26/defining_interface/interface2.go
@@ -1,11 +1,20 @@
 package main 
 
-type MyInterface interface {
-	methodWithoutParameters()
-	methodWithParameter(float64)
-	methodWithReturnValue() string 
-}
+import (
+	"golang_tutorial/class_26/mypkg"
+	"fmt"
+)
 
 func main(){
-
+	/*A variable declared with an interface type can hold any value whose type
+	satisfies that interface. This code declares a value variable with MyInterface as
+	its type, then creates a MyType value and assigns it to value. (Which is allowed,
+	because MyType satisfies MyInterface.) Then we call all the methods on that
+	value that are part of the interface.*/
+	
+	var value mypkg.MyInterface
+	value = mypkg.MyType(6)
+	value.MethodWithParameter(12.1)
+	value.MethodWithoutParameters()
+	fmt.Println(value.MethodWithReturnValue())
 }
