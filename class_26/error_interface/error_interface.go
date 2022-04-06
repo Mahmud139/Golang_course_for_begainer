55b4d2a6d1f19f7edf41bbc1fceeaee64e9b58f1
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-06 20:04:14 +0600

    type error interface

--- class_26/error_interface/error_interface.go
+++ class_26/error_interface/error_interface.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+type CommedyError string
+func (c CommedyError) Error() string {
+	return string(c)
+}
+
 func main() {
-	err := fmt.Errorf("a height of %0.2f is invalid", -2.333)
-	fmt.Println(err.Error())
+	// err := fmt.Errorf("a height of %0.2f is invalid", -2.333)
+	// fmt.Println(err.Error())
+	// fmt.Println(err)
+
+	var err error 
+	err = CommedyError("What's a programmer's favorite food?")
 	fmt.Println(err)
 }
