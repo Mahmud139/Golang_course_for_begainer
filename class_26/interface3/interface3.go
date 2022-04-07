d8893b714b842a55118619eb168b10fab9921d01
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-07 19:55:47 +0600

    error interface

--- class_26/interface3/interface3.go
+++ class_26/interface3/interface3.go
@@ -4,32 +4,44 @@ import "fmt"
 
 type Whistel string
 
-func (w Whistel) MakeSound() {
-	fmt.Println("Tweet!")
+// func (w Whistel) MakeSound() {
+// 	fmt.Println("Tweet!")
+// }
+
+func (w Whistel) MakeSound() string {
+	return fmt.Sprintf("Tweet! to %s",w)
 }
 
-type Horn string
+//type Horn string
 
-func (h Horn) MakeSound() {
-	fmt.Println("Honk!")
-}
+// func (h Horn) MakeSound() {
+// 	fmt.Println("Honk!")
+// }
 
 type NoiseMaker interface {
-	MakeSound()
+	MakeSound() string
 }
 
-func play(n NoiseMaker) {
-	n.MakeSound()
+// func play(n NoiseMaker) {
+// 	n.MakeSound()
+// }
+
+type test float64
+func (t test) Error() string {
+	return fmt.Sprintf("test %f form error", t)
 }
 
 func main() {
-	var toy NoiseMaker
-	toy = Whistel("mahmud")
-	toy.MakeSound()
+	var toy NoiseMaker = Whistel("mahmud")
+	fmt.Println(toy)
 
-	toy = Horn("hasan")
-	toy.MakeSound()
+	var err error = test(12.1)
+	fmt.Println(err)
+	//toy.MakeSound()
 
-	play(Whistel("mahmud"))
-	play(Horn("hasan"))
+	// toy = Horn("hasan")
+	// toy.MakeSound()
+
+	// play(Whistel("mahmud"))
+	// play(Horn("hasan"))
 }
