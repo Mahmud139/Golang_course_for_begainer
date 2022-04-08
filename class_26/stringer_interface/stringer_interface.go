4742fa3af6699f1dcafdc0c9466cfa1fbe15f8bb
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-08 19:51:54 +0600

    stringer interface

---
+++ class_26/stringer_interface/stringer_interface.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Gallons float64
+type Liters float64
+type Milliliters float64
+
+type Coffepot string
+func (c Coffepot) String() string {
+	return string(c) + " coffe pot"
+}
+
+func main(){
+	// fmt.Println(Gallons(12.12121))
+	// fmt.Println(Liters(12.12121))
+	// fmt.Println(Milliliters(12.12121))
+
+	// fmt.Printf("%0.2f gal\n",Gallons(12.1212))
+	// fmt.Printf("%0.2f L\n",Liters(12.12121))
+	// fmt.Printf("%0.2f ml\n",Milliliters(12.12121))
+
+	coffepot := Coffepot("Mahmud")
+	fmt.Println(coffepot.String())
+}
