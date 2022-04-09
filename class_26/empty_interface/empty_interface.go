84c0ed5801860e41325a6f595afe07d485cadf09
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-09 13:57:56 +0600

    empty interface

--- class_26/empty_interface/empty_interface.go
+++ class_26/empty_interface/empty_interface.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// type Anything interface {
+
+// }
+
+func AcceptAnything(thing interface{}) {
+	fmt.Println(thing)
+}
+
 func main(){
-	fmt.Println(12, 12.121, "Mahmud", true)
+	//fmt.Println(12, 12.121, "Mahmud", true)
+
+	AcceptAnything(321.12121)
+	AcceptAnything("Hasan")
 }
