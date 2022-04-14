8f32c8e108e5b4e242006fa859af7d13230fe0b3
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-14 19:46:09 +0600

    recovering from errors using deferred function calls

--- class_27/defering_func_call/defering_func_call.go
+++ class_27/defering_func_call/defering_func_call.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func Socialize() {
+// func Socialize() {
+// 	defer fmt.Println("Goodbye")
+// 	fmt.Println("Hello!")
+// 	fmt.Println("Nice Weather, eh?")
+// }
+
+func Socialize() error {
 	defer fmt.Println("Goodbye")
 	fmt.Println("Hello!")
+	return fmt.Errorf("I don't want to talk.")
 	fmt.Println("Nice Weather, eh?")
+	return nil 
 }
+	/*because we include a defer keyword before the
+	fmt.Println("Goodbye!") call, Socialize will always be polite enough to
+	print “Goodbye!” before ending the conversation.*/
 
 func main(){
-	Socialize()
+	err := Socialize()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
