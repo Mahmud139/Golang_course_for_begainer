8bf4b79d2c3f7d0b67cb85a3889b920b21151e8e
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-18 21:37:04 +0600

    recover function

---
+++ class_27/recover_function2/recover_function2.go
@@ -0,0 +1,18 @@
+/*If you call recover when a program is panicking, it will stop the panic. But
+when you call panic in a function, that function stops executing. So there’s no
+point calling recover in the same function as panic, because the panic will
+continue anyway:*/
+
+package main
+
+import "fmt"
+
+func main(){
+	freckOut()
+	fmt.Println("Exiting Normally")
+}
+
+func freckOut() {
+	panic("Oh no!")
+	recover()
+}
