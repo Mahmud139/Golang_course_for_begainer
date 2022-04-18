6f1b1ece9f867201b107353c7491d07a7deecdc2
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-18 19:44:07 +0600

    deferred function calls completed before crash

--- class_27/deffered_func_call/deffered_func_call.go
+++ class_27/deffered_func_call/deffered_func_call.go
@@ -16,3 +16,6 @@ func two(){
 	panic("Let's see what's been defered!")
 }
 
+/*When a program panics, all deferred function calls will still be made. If there’s
+more than one deferred call, they’ll be made in the reverse of the order they were
+deferred in.*/
