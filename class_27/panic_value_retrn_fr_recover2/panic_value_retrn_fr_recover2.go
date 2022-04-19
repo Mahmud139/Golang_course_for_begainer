0e0d478f99fc0cc724b8535cb77dbeb3b5fab4f6
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-19 14:26:38 +0600

    the panic value returned from recover

--- class_27/panic_value_retrn_fr_recover2/panic_value_retrn_fr_recover2.go
+++ class_27/panic_value_retrn_fr_recover2/panic_value_retrn_fr_recover2.go
@@ -10,5 +10,9 @@ func main() {
 
 func calmDown() {
 	p := recover()
-	fmt.Println(p.Error())
+	//fmt.Println(p.Error())
+	/*To call methods or do anything else with the panic value, you’ll need to convert
+it back to its underlying type using a type assertion.*/
+	err := p.(error)
+	fmt.Println(err.Error())
 }
