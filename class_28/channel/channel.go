67693296c862d1529f97483227cff2dc1b82e3a2
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-21 13:53:16 +0600

    Go statement can't be use with return values

---
+++ class_28/channel/channel.go
@@ -0,0 +1,35 @@
+//Go statements can’t be used with return values
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func responseSize(url string) int {
+	fmt.Println("Getting",url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return len(body)
+}
+
+func main() {
+	var size int
+	size = go responseSize("https://example.com")
+	fmt.Println(size)
+	size = go responseSize("https://golang.org/")
+	fmt.Println(size)
+	size = go responseSize("https://golang.org/doc")
+	fmt.Println(size)
+	time.Sleep(7*time.Second)
+}
