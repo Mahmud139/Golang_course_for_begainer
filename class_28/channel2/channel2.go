793ffea7520c40d12bcc57c0fd8d31ea6d208ccc
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-21 14:10:30 +0600

    Go statement can't be use with return values

--- class_28/channel2/channel2.go
+++ class_28/channel2/channel2.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
+//import "fmt"
 
 func main() {
-	fmt.Println(go greetings())
-	//go greetings()
+	//fmt.Println(go greetings())
+	go greetings()
 }
 
 func greetings() string {
