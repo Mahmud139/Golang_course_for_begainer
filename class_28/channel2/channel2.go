1ab7f8c28dd2d33314912439dc32026ab723108f
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-21 16:19:15 +0600

    declaring channel

--- class_28/channel2/channel2.go
+++ class_28/channel2/channel2.go
@@ -10,3 +10,7 @@ func main() {
 func greetings() string {
 	return "Hi"
 }
+
+/*Go won’t let you use the return value from a function called with a go statement, 
+because there’s no guarantee the return value will be ready before we attempt to use it:
+*/
---
+++ class_28/channel3/channel3.go
@@ -0,0 +1,12 @@
+//declaraing channel
+package main
+
+import "fmt"
+
+func main(){
+	// var myChannel chan float64
+	// myChannel = make(chan float64)
+
+	myChannel := make(chan float64)
+	fmt.Println(myChannel)
+}
