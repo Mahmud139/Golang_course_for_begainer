d18f19ff41652df7818430c2bc9a17fe44902704
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-24 14:38:38 +0600

    channel

--- class_28/channel4/channel4.go
+++ class_28/channel4/channel4.go
@@ -1,24 +1,24 @@
 package main
 
 import "fmt"
-var myChannel = make(chan string)
+//var myChannel = make(chan string)
 
 func main() {
-	// myChannel := make(chan string)
-	// go greetings(myChannel)
+	myChannel := make(chan string)
+	go greetings(myChannel)
 	//fmt.Println(<- myChannel)
 
-	// receiveValue := <- myChannel
-	// fmt.Println(receiveValue)
-	test()
+	receiveValue := <- myChannel
+	fmt.Println(receiveValue)
+	//test()
 }
 
 func greetings(myChannel chan string) {
 	myChannel <- "Hi"
 }
 
-func test() {
-	//myChannel := make(chan string)
-	go greetings(myChannel)
-	fmt.Println(<- myChannel)
-}
+// func test() {
+// 	//myChannel := make(chan string)
+// 	go greetings(myChannel)
+// 	fmt.Println(<- myChannel)
+// }
