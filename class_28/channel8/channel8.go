6715a8a85b7aed6acc262756ce5190cb35a7faef
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-24 20:53:26 +0600

    observing goroutine synchronization

--- class_28/channel8/channel8.go
+++ class_28/channel8/channel8.go
@@ -3,19 +3,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func reportNap(name string, delay int) {
-	for i := 0; i < delay; i++ {
-		fmt.Println(name, "Sleeping")
-		time.Sleep(1 * time.Second)
-	}
-	fmt.Println(name, "Wakes up!")
-}
-
 func send(myChannel chan string) {
-	reportNap("sending goroutine", 2)
 	fmt.Println("***sending value***")
 	myChannel <- "a"
 	fmt.Println("***sending value***")
@@ -25,7 +15,6 @@ func send(myChannel chan string) {
 func main() {
 	myChannel := make(chan string)
 	go send(myChannel)
-	reportNap("receiving goroutine", 5)
 	fmt.Println(<- myChannel)
 	fmt.Println(<- myChannel)
 }
