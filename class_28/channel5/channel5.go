338d228c4f082bad9bd5a6e2bc162d2f8015bb74
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-24 14:12:47 +0600

    synchronizing goroutine with channels

---
+++ class_28/channel5/channel5.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myChannel1 := make(chan string)
+	myChannel2 := make(chan string)
+	go abc(myChannel1)
+	go def(myChannel2)
+	fmt.Println(<- myChannel1)
+	fmt.Println(<- myChannel2)
+}
+
+func abc(channel chan string) {
+	channel <- "a"
+	channel <- "b"
+	channel <- "c"
+}
+
+func def(channel chan string) {
+	channel <- "d"
+	channel <- "e"
+	channel <- "f"
+}
