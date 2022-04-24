6e50ee6d9c11c752518a6ae5f13543e40166f875
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-24 14:20:49 +0600

    synchronizing goroutine with channels

--- class_28/channel5/channel5.go
+++ class_28/channel5/channel5.go
@@ -9,11 +9,12 @@ func main() {
 	myChannel1 := make(chan string)
 	myChannel2 := make(chan string)
 	go abc(myChannel1)
+	//fmt.Println(<- myChannel2) //channels also ensure the sending goroutine has sent the value before the receiving channel attempts to use it.
 	go def(myChannel2)
 	fmt.Println(<- myChannel1)
-	fmt.Println(<- myChannel1)
-	fmt.Println(<- myChannel1)
-	fmt.Println(<- myChannel1)
+	// fmt.Println(<- myChannel1)
+	// fmt.Println(<- myChannel1)
+	// fmt.Println(<- myChannel1)
 	fmt.Println(<- myChannel2)
 }
 
