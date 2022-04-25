5b1f98322a6d52b9ea4e57e845996e6c5b39c203
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-25 20:06:36 +0600

    a function for automated tests

---
+++ class_29/autoTest/autoTest.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"golang_tutorial/class_29/prose"
+	"fmt"
+)
+
+func main(){
+	fruits := []string{"mango", "apple", "pear", "banana"}
+	fmt.Println(prose.JoinWithCommas(fruits))
+	phrases := []string{"my parents", "a rodeo clown"}
+	fmt.Println("a photo of", prose.JoinWithCommas(phrases))
+	phrases = []string{"my parents", "a rodeo clown", "prize bull"}
+	fmt.Println("my parents", prose.JoinWithCommas(phrases))
+}
