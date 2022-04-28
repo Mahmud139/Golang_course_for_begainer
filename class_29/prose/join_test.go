66dc737f891ab9c3a91297b949a0aa3bd156eef3
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-28 13:45:03 +0600

    automated testing

--- class_29/prose/join_test.go
+++ class_29/prose/join_test.go
@@ -4,8 +4,16 @@ import "testing"
 
 func TestTwoElements(t *testing.T) {
 	//t.Error("no test written yet")
+	list := []string{"apple", "orange"}
+	if JoinWithCommas(list) != "apple and orange" {
+		t.Error("didn't match expected value")
+	}
 }
 
 func TestThreeElement(t *testing.T) {
 	//t.Error("no test here either")
+	list := []string{"apple", "orange", "pear"}
+	if JoinWithCommas(list) != "apple and orange, and pear" {
+		t.Error("didn't match expected value")
+	}
 }
