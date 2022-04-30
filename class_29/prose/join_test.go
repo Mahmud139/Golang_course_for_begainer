36d149a115c2ca95aad3de11489c476eef166bb2
mahmud139 <mahmudulhasan139139@gmail.com>
2022-04-30 20:11:45 +0600

    aumated testing

--- class_29/prose/join_test.go
+++ class_29/prose/join_test.go
@@ -2,18 +2,37 @@ package prose
 
 import "testing"
 
+// func TestTwoElements(t *testing.T) {
+// 	//t.Error("no test written yet")
+// 	list := []string{"apple", "orange"}
+// 	if JoinWithCommas(list) != "apple and orange" {
+// 		t.Error("didn't match expected value")
+// 	}
+// }
+
+// func TestThreeElement(t *testing.T) {
+// 	//t.Error("no test here either")
+// 	list := []string{"apple", "orange", "pear"}
+// 	if JoinWithCommas(list) != "apple, orange, and pear" {
+// 		t.Error("didn't match expected value")
+// 	}
+// }
+
+
 func TestTwoElements(t *testing.T) {
-	//t.Error("no test written yet")
 	list := []string{"apple", "orange"}
-	if JoinWithCommas(list) != "apple and orange" {
-		t.Error("didn't match expected value")
+	want := "apple and orange"
+	got := JoinWithCommas(list)
+	if  got != want {
+		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",list, got, want)
 	}
 }
 
 func TestThreeElement(t *testing.T) {
-	//t.Error("no test here either")
 	list := []string{"apple", "orange", "pear"}
-	if JoinWithCommas(list) != "apple, orange, and pear" {
-		t.Error("didn't match expected value")
+	want := "apple, orange, and pear"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",list, got, want)
 	}
 }
