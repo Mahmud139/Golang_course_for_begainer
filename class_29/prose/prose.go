0c8079e21bcecc136332d123085734825a4b981b
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-08 20:33:54 +0600

    getting the tests to passed

--- class_29/prose/prose.go
+++ class_29/prose/prose.go
@@ -2,9 +2,21 @@ package prose
 
 import "strings"
 
+// func JoinWithCommas(phrases []string) string {
+// 	result := strings.Join(phrases[:len(phrases)-1], ", ")
+// 	result += ", and "
+// 	result += phrases[len(phrases)-1]
+// 	return result
+	
+// }
+
 func JoinWithCommas(phrases []string) string {
-	result := strings.Join(phrases[:len(phrases)-1], ", ")
+	if len(phrases) == 2 {
+		return phrases[0] + " and " + phrases[1]
+	} else {
+		result := strings.Join(phrases[:len(phrases)-1], ", ")
 	result += ", and "
 	result += phrases[len(phrases)-1]
 	return result
+	}
 }
