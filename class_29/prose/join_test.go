206652a08ca32b17aa12920bed59a2b597c2ba44
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-09 20:48:01 +0600

    table driven test

--- class_29/prose/join_test.go
+++ class_29/prose/join_test.go
@@ -1,7 +1,7 @@
 package prose
 
 import (
-	"fmt"
+	//"fmt"
 	"testing"
 )
 
@@ -40,33 +40,53 @@ import (
 // 	}
 // }
 
-func TestOneElement(t *testing.T) {
-	list := []string{"apple"}
-	want := "apple"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Errorf(errorString(list, got, want))
-	}
+// func TestOneElement(t *testing.T) {
+// 	list := []string{"apple"}
+// 	want := "apple"
+// 	got := JoinWithCommas(list)
+// 	if got != want {
+// 		t.Errorf(errorString(list, got, want))
+// 	}
+// }
+
+// func TestTwoElements(t *testing.T) {
+// 	list := []string{"apple", "orange"}
+// 	want := "apple and orange"
+// 	got := JoinWithCommas(list)
+// 	if  got != want {
+// 		t.Errorf(errorString(list, got, want))
+// 	}
+// }
+
+// func TestThreeElements(t *testing.T) {
+// 	list := []string{"apple", "orange", "pear"}
+// 	want := "apple, orange, and pear"
+// 	got := JoinWithCommas(list)
+// 	if got != want {
+// 		t.Errorf(errorString(list, got, want))
+// 	}
+// }
+
+// func errorString(list []string, got string, want string) string {
+// 	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",list, got, want)
+// }
+
+type testData struct {
+	list []string
+	want string
 }
 
-func TestTwoElements(t *testing.T) {
-	list := []string{"apple", "orange"}
-	want := "apple and orange"
-	got := JoinWithCommas(list)
-	if  got != want {
-		t.Errorf(errorString(list, got, want))
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData {
+		{list:[]string{"apple"},want:"apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "banana"}, want: "apple, orange, and banana"},
 	}
-}
 
-func TestThreeElements(t *testing.T) {
-	list := []string{"apple", "orange", "pear"}
-	want := "apple, orange, and pear"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Errorf(errorString(list, got, want))
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",test.list, got, test.want)
+		}
 	}
-}
-
-func errorString(list []string, got string, want string) string {
-	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"",list, got, want)
 }
