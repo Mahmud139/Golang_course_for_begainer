34f2fa42d12347f90c65e7722f756cac2c1c5fcc
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-15 20:17:43 +0600

    different resource path

---
+++ class_30/web_app2/web_app2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web")
+}
+func banglaHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "kire, web")
+}
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "namaste, web")
+}
+func write(write http.ResponseWriter, s string) {
+	message := []byte(s)
+	_, err := write.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello",englishHandler)
+	http.HandleFunc("/kire", banglaHandler)
+	http.HandleFunc("/namaste",hindiHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
