7ed0d5d2e1d2fb74d28d8de5f650afe9d6d99e4c
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-17 20:19:32 +0600

    inserting data using template action

--- class_31/inserting_data/inserting_data.go
+++ class_31/inserting_data/inserting_data.go
@@ -12,14 +12,21 @@ func check(err error) {
 	}
 }
 
+func executeTemplate(text string, data interface{}) {
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+}
+
 func main() {
-	templateText := "Template start\nAction: {{.}}\nTemplate end\n"
-	tmpl, err := template.New("test").Parse(templateText)
-	check(err)
-	err = tmpl.Execute(os.Stdout, "ABC")
-	check(err)
-	err = tmpl.Execute(os.Stdout, 1234)
-	check(err)
-	err = tmpl.Execute(os.Stdout, true)
-	check(err)
+	// templateText := "Template start\nAction: {{.}}\nTemplate end\n"
+	// tmpl, err := template.New("test").Parse(templateText)
+	// check(err)
+	// err = tmpl.Execute(os.Stdout, "ABC")
+	// check(err)
+	// err = tmpl.Execute(os.Stdout, 1234)
+	// check(err)
+	// err = tmpl.Execute(os.Stdout, true)
+	// check(err)
+	
 }
