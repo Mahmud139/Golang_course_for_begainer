79b47cb7ac37b1d88c403cfca834278173ad6752
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-18 15:58:53 +0600

    inserting struct fields into a template with actions

--- class_31/inserting_data4/inserting_data4.go
+++ class_31/inserting_data4/inserting_data4.go
@@ -1,3 +1,4 @@
+//Inserting struct fields into a template with actions
 package main
 
 import (
@@ -27,5 +28,8 @@ func main() {
 	text := "Name: {{.Name}}\nCount: {{.Count}}\n"
 	executeTemplate(text, Part{Name: "Bolt", Count: 12})
 	executeTemplate(text, Part{Name: "Cables", Count: 23})
-
 }
+
+/*If the value in dot is a struct, then an action with dot followed by a field name 
+will insert that field’s value in the template. Here we create a Part struct type, 
+then set up a template that will output a Part value’s Name and Count fields:*/
---
+++ class_31/inserting_data5/inserting_data5.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func executeTemplate(text string, data interface{}){
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+}
+
+type Subscriber struct{
+	Name string
+	Rate float64
+	Active bool
+}
+
+func main() {
+	text := "Name: {{.Name}}\n{{if .Active}}Rate: ${{.Rate}}\n{{end}}\n"
+	subscriber := Subscriber{Name: "Mahmud", Rate: 5.99, Active: true}
+	executeTemplate(text, subscriber)
+	subscriber = Subscriber{Name: "Mahmud", Rate: 5.99, Active: false}
+	executeTemplate(text, subscriber)
+}
