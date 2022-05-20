1614098b0548f179dd909f037213c7c986427f3c
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-20 19:58:21 +0600

    os.filemode

--- class_32/filemode/filemode.go
+++ class_32/filemode/filemode.go
@@ -9,6 +9,7 @@ func main() {
 	fmt.Println(os.FileMode(0700))
 	fmt.Println(os.FileMode(0070))
 	fmt.Println(os.FileMode(0007))
+	fmt.Println(os.FileMode(0707))
 }
 
 /*Each file has three sets of permissions, affecting three different classes of users.
