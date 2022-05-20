06efbb4eece3d7b085d39dc1f20fa0673d39945c
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-20 19:57:36 +0600

    os.filemode

---
+++ class_32/filemode/filemode.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.FileMode(0700))
+	fmt.Println(os.FileMode(0070))
+	fmt.Println(os.FileMode(0007))
+}
+
+/*Each file has three sets of permissions, affecting three different classes of users.
+The first set of permissions applies only to the user that owns the file. (By
+default, your user account is the owner of any files you create.) The second set
+of permissions is for the group of users that the file is assigned to. And the third
+set applies to other users on the system that are neither the file owner nor part of
+the file’s assigned group.
+FileMode*/
