2f81c367bb08c61beaef2ff86c051097f3c4c60b
mahmud139 <mahmudulhasan139139@gmail.com>
2023-04-25 11:29:42 +0800

    updated type switch

--- class_8/type_switch/type_switch.go
+++ class_8/type_switch/type_switch.go
@@ -10,12 +10,15 @@ func main(){
 		fmt.Println("value is boolean type.")
 	case int :
 		fmt.Println("value is integer type.")
+		fmt.Printf("%T", a)
 	case float64 :
 		fmt.Println("value is float64 type.")
 	default :
 		fmt.Printf("value is of type: %T",a)
 	}
 
-
+	//we cann't use this kinda statemaent outside of "type switch"
+	// n := shihab.(type)
+	// fmt.Printf("%T", n)
 }
 
