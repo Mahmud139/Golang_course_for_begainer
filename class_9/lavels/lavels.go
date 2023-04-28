04358cf08b75a1099108e37e6a08094472559e80
mahmud139 <mahmudulhasan139139@gmail.com>
2023-04-28 14:52:59 +0800

    updated

--- class_9/lavels/lavels.go
+++ class_9/lavels/lavels.go
@@ -10,18 +10,19 @@ func main() {
     //     for j := 1; j < 3; j++ {
     //         fmt.Printf("i = %d , j = %d\n", i, j)
     //         if i == j {
-    //             break
+    //             break //here we want to break out the outer loop, but it's just 
+						//break the current loop. what's the solution?
     //         }
     //     }
     // }
 
 
-	//solution 
+	//solution : we can use level to accomplish our requirement
 	outer:  
 		for i := 0; i < 3; i++ {
 				for j := 1; j < 4; j++ {
 					fmt.Printf("i = %d , j = %d\n", i, j)
-					if j == 1 {
+					if i == j {
 						break outer
 					}
 				}
