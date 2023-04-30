b8e6f521dee00c889026bceb87bfe8ad346e19bd
mahmud139 <mahmudulhasan139139@gmail.com>
2023-04-30 15:08:22 +0800

    updated

--- class_12/anonymous_function2/anonymous2.go
+++ class_12/anonymous_function2/anonymous2.go
@@ -5,24 +5,29 @@ import "fmt"
 
 func main(){
 	//with parameter and no return value 
-	// area := func(height int, width int)  {
-	// 	fmt.Println("The area is", height*width)
-	// }
-	// area(12,12)
-	// fmt.Printf("The type of a() is: %T", area)
+	area := func(height int, width int)  {
+		fmt.Println("The area is", height*width)
+	}
+	area(12,12)
+	fmt.Printf("The type of a() is: %T", area)
 
 	//with parameter and return value
-	area := func(height int, width int) int {
+	area2 := func(height int, width int) int {
 		return height*width
 	}
 	//area(12,12) //in this time we can't see the output value in our screen 
 				// so we need to assign this variable in our Println function. 
 	//fmt.Println(area(12,12))
-	res := area(12,12)
+	res := area2(12,12)
 	fmt.Println(res)
-	fmt.Printf("The type of a() is: %T", area)
+	fmt.Printf("The type of a() is: %T\n", area)
 
-	// no paremeter but have return value (home work)
+	// no paremeter but have return value
+	name := func() string {
+		return "Mahmud"
+	}
 
+	result := name()
+	fmt.Println(result)
 
 }
