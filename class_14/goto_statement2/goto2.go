00f0320632e05cc50207913f93dc9df4673ed87b
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-02 20:47:09 +0800

    updated

--- class_14/goto_statement2/goto2.go
+++ class_14/goto_statement2/goto2.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-
 )
 
 func main(){
-	fmt.Println("Befor execution of goto statement.")
+	fmt.Println("Before execution of goto statement.")
 	i:=1
 	START: 	
 		for i<=5 {
 			if i%2 == 0 {
 				i++
-				goto START
+				goto START //we can achieve the same goal using "continue statement"
+				// continue
 			}
 			fmt.Println(i)
 			i++
