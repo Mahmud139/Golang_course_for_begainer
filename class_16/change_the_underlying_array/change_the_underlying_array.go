60b6f38fa660d0533354f51044c274078c2c59ba
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-04 00:39:42 +0800

    updated

--- class_16/change_the_underlying_array/change_the_underlying_array.go
+++ class_16/change_the_underlying_array/change_the_underlying_array.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	// slide no 17
-	// array1 := [5]string{"a", "b", "c", "d", "e"}
-	// slice1 := array1[0:3]
-	// fmt.Println(slice1)
-	// array1[1] = "X"
-	// fmt.Println(array1)
-	// fmt.Println(slice1)
+	// slide no 18
+	array1 := [5]string{"a", "b", "c", "d", "e"}
+	slice1 := array1[0:3]
+	fmt.Println(slice1)
+	array1[1] = "X"
+	fmt.Println(array1)
+	fmt.Println(slice1)
 
-	//slide no 18
+	//slide no 19
 	// array2 := [5]string{"f", "g", "h", "i", "j"}
 	// slice2 := array2[2:5]
 	// fmt.Println(slice2)
@@ -20,12 +20,12 @@ func main(){
 	// fmt.Println(slice2)
 
 
-	array3 := [5]string{"a", "b", "c", "d", "e"}
-	slice3 := array3[0:3]
-	slice4 := array3[2:5]
-	fmt.Println(slice3, slice4)
-	slice3[2] = "X"
-	//array3[2] = "X"
-	fmt.Println(array3)
-	fmt.Println(slice3, slice4)
+	// array3 := [5]string{"a", "b", "c", "d", "e"}
+	// slice3 := array3[0:3]
+	// slice4 := array3[2:5]
+	// fmt.Println(slice3, slice4)
+	// slice3[2] = "X"
+	// //array3[2] = "X"
+	// fmt.Println(array3)
+	// fmt.Println(slice3, slice4)
 }
