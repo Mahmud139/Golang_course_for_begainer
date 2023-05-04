df52d97e0597c9ca5eb283f5edbccddcf3dec3ea
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-04 12:16:25 +0800

    updated

--- class_16/zero_value_of_slice/zero_value_of_slice.go
+++ class_16/zero_value_of_slice/zero_value_of_slice.go
@@ -3,17 +3,18 @@ package main
 import "fmt"
 
 func main(){
-	// slide no 24
-	floatSlice := make([]float64,10)
-	boolSlice := make([]bool, 10)
-	fmt.Println(floatSlice, boolSlice[5])
+	// slide no 25
+	// floatSlice := make([]float64,10)
+	// boolSlice := make([]bool, 10)
+	// fmt.Println(floatSlice, boolSlice[5])
 
-	//var intSlice []int
-	// var stringSlice []string
-	// fmt.Println(intSlice, stringSlice)
-	// fmt.Printf("intSlice: %#v, stringSlice: %#v",intSlice, stringSlice)
+	var intSlice []int
+	var stringSlice []string
+	emptySlice := []int{} //initialized an empty slice with slice literal
+	fmt.Println(intSlice, stringSlice, emptySlice)
+	fmt.Printf("intSlice: %#v, stringSlice: %#v, emptySlice: %#v",intSlice, stringSlice, emptySlice)
 
-	//slide no 25
+	//slide no 26
 	//intSlice[0] = 5
 	//fmt.Println(intSlice)
 	// fmt.Println(len(intSlice))
@@ -21,7 +22,9 @@ func main(){
 	// intSlice = append(intSlice, 25)
 	// fmt.Printf("intSlice: %#v\n",intSlice)
 	// fmt.Println(len(intSlice), cap(intSlice))
-	//slide no 26
+
+
+	//slide no 27
 	// var slice []string
 	// if len(slice) == 0 {
 	// 	slice = append(slice, "first item")
