da731bd31276229de7339cf950f333082a7408ee
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-04 22:55:42 +0800

    updated

--- class_16/passing_slice_to_function/passing_slice_to_function.go
+++ class_16/passing_slice_to_function/passing_slice_to_function.go
@@ -15,3 +15,6 @@ func main() {
     subtactOne(nos)                               
     fmt.Println("slice after function call", nos) 
 }
+
+/*A slice contains the length, capacity and a pointer to the zeroth element of the underlying array. When a slice is passed to a function, even though it's passed by value, the pointer variable will refer to the same underlying array. Hence when a slice is passed to a function as parameter, changes made inside the function are visible outside the function too. Let's write a program to check this out.
+*/
