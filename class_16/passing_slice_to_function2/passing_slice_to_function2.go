b76d6297bd716cbc5d8d4e6bd8342d644067e408
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-04 23:33:46 +0800

    updated

--- class_16/passing_slice_to_function2/passing_slice_to_function2.go
+++ class_16/passing_slice_to_function2/passing_slice_to_function2.go
@@ -5,11 +5,21 @@ import "fmt"
 func change(s []string) {
 	s[0] = "Go"
 	s = append(s, "playground")
-	fmt.Println(s)
+	fmt.Println("inside the function: ",s)
+	//Q1: why the result is bit different between main() and this function?
+
+
+	// s = append(s, "playground")
+	// s[0] = "Go"
+	// fmt.Println("inside the function: ",s)
+	//Q2: why the result between main() and this function is completely different?
 }
 
 func main() {
 	welcome := []string{"hello", "world"}
+	fmt.Println("Before calling the function: ", welcome)
 	change(welcome)
-	fmt.Println(welcome)
+	fmt.Println("after calling the function: ",welcome)
 }
+
+//to understand the above question, you must have to understand how slice work.
