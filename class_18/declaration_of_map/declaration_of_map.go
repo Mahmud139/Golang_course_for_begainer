181aaadec6e54b5f674008b05978547b3b17a670
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-09 21:49:25 +0800

    updated

--- class_18/declaration_of_map/declaration_of_map.go
+++ class_18/declaration_of_map/declaration_of_map.go
@@ -1,12 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var ranks map[string]int
 	ranks = make(map[string]int)
 
-	//name := make(map[string]int)
+	//name := make(map[string]int) //short-hand declaration
 
 	ranks["gold"] = 1
 	ranks["silver"] = 2
@@ -14,4 +16,18 @@ func main() {
 	fmt.Println(ranks["gold"])
 	fmt.Println(ranks["silver"])
 	fmt.Println(ranks)
+
+
+	//in map the key is Alpha-numerically ordered
+	// order := make(map[string]int)
+	// order["b"] = 2
+	// order["a"] = 1
+	// order["c"] = 3
+	// fmt.Println(order)
+
+	// order2 := make(map[int]int)
+	// order2[2] = 2
+	// order2[1] = 11
+	// order2[3] = 33
+	// fmt.Println(order2)
 }
