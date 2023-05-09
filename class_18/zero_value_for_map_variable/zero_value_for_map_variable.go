29e949afe44afb64e53bb2836d466b1e2a3bafd6
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-09 22:06:45 +0800

    updated

--- class_18/zero_value_for_map_variable/zero_value_for_map_variable.go
+++ class_18/zero_value_for_map_variable/zero_value_for_map_variable.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	// var nilMap map[int]string
-	// fmt.Printf("%#v\n",nilMap)
-	// nilMap[3] = "three"
+	var nilMap map[int]string //nill map
+	fmt.Printf("%#v\n",nilMap)
+	nilMap[3] = "three" //we can't assign to nill map
+	fmt.Println(nilMap)
 
-	var myMap map[int]string = make(map[int]string)
-	myMap[3] = "three"
-	fmt.Printf("%#v\n",myMap)
+	//this will work fine
+	// var myMap map[int]string = make(map[int]string)
+	// myMap[3] = "three"
+	// fmt.Printf("%#v\n",myMap)
+
+	//but we can assing to empty map
+	// emptyMap := map[string]int{} //empty map
+	// fmt.Printf("%#v\n",emptyMap)
+	// emptyMap["roll"] = 12
+	// fmt.Println(emptyMap)
 }
