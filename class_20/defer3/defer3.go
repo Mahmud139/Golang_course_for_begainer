d8f86c6ec63c7a86f98d81e27ec5cf0dce13ae57
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-10 16:54:59 +0800

    updated

--- class_20/defer3/defer3.go
+++ class_20/defer3/defer3.go
@@ -5,6 +5,7 @@ import "fmt"
 func main(){
 	a := 5
 	defer printA(a)
+	/*The arguments of a deferred function are evaluated when the defer statement is executed and not when the actual function call is done.*/
 	a = 10
 	fmt.Println("value of a before deferred function call", a)
 }
