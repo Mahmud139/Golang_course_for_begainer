966d036de3e558383f8b69797f4462fd459fab33
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-10 17:01:48 +0800

    updated

--- class_20/defer4/defer4.go
+++ class_20/defer4/defer4.go
@@ -5,11 +5,25 @@ import "fmt"
 func main(){
 	res:= foo()
 	fmt.Println(res)
+
+	// res2:= bar()
+	// fmt.Println(res2)
 }
 
+/*Deferred anonymous functions may access and modify the surrounding function’s "named return" parameters.*/
 func foo() (result string) {
 	defer func ()  {
 		result = "change world"
 	}()
 	return "hello world"
 }
+
+
+/*Deferred anonymous functions can't access and modify the surrounding function’s return parameters.*/
+// func bar() string {
+// 	result := "hello world"
+// 	defer func ()  {
+// 		result = "change world"
+// 	}()
+// 	return result
+// }
