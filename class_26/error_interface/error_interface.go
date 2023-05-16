a816f15054e01c2a1fbb3a6d2db33a885cf50ae3
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-16 09:39:59 +0800

    updated

--- class_26/error_interface/error_interface.go
+++ class_26/error_interface/error_interface.go
@@ -7,7 +7,7 @@ import (
 
 type OverheatError float64
 func (o OverheatError) Error() string {
-	return fmt.Sprintf("Overheating by %0.2f degrees!", o)
+	return fmt.Sprintf("Overheating by mahmud %0.2f degrees!", o)
 }
 
 func CheckTemperature(actual float64, safe float64) error {
@@ -21,10 +21,16 @@ func CheckTemperature(actual float64, safe float64) error {
 func main() {
 	// err := fmt.Errorf("a height of %0.2f is invalid", -2.333)
 	// fmt.Println(err.Error())
-	// fmt.Println(err)
+	// fmt.Println(err) //here Go automatically call Error() method itself
 
 	var err error = CheckTemperature(121.45, 100.0)
+	// var err error = OverheatError(21.45)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// var a int = 12
+	// var b int = a
 }
+
+/*In Go, it is not mandatory to use the Error method specifically when printing errors. When an error is printed using the fmt.Println or fmt.Printf functions, Go automatically calls the Error method if the error value implements the error interface. */
