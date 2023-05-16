7eb543e7961500a8fa70d84e838598d58dc78a66
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-16 19:48:11 +0800

    updated

--- class_26/empty_interface/empty_interface.go
+++ class_26/empty_interface/empty_interface.go
@@ -2,9 +2,7 @@ package main
 
 import "fmt"
 
-// type Anything interface {
-
-// }
+type Anything interface {}
 
 func AcceptAnything(thing interface{}) {
 	fmt.Println(thing)
@@ -27,6 +25,11 @@ func AcceptHorn(horn Horn) {
 
 func main(){
 	//fmt.Println(12, 12.121, "Mahmud", true)
+
+	// var any Anything = Horn("poooooo")
+	// any.MakeSound()
+	/*Don’t try calling any methods on an empty-interface value! Remember, if you have a value with an interface type, you can only call methods on it that are part of the interface. And the empty interface doesn’t have any methods. That means there are no methods you can call on a value with the empty interface type!*/
+
 
 	AcceptAnything(321.12121)
 	AcceptAnything("Hasan")
