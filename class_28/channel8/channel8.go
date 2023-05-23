c6f902d9409aa3a132aa7874cd135c6e7014ec62
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-23 15:05:26 +0800

    updatd

--- class_28/channel8/channel8.go
+++ class_28/channel8/channel8.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	// "time"
 )
 
 func send(myChannel chan string) {
@@ -10,6 +11,8 @@ func send(myChannel chan string) {
 	myChannel <- "a"
 	fmt.Println("***sending value***")
 	myChannel <- "b"
+	fmt.Println("***Finish!***")
+	/*this won't run because of as soon as main function receives both channel value it done executing, So there's no time or opportunity for this line of code to execute.*/
 }
 
 func main() {
@@ -17,4 +20,6 @@ func main() {
 	go send(myChannel)
 	fmt.Println(<- myChannel)
 	fmt.Println(<- myChannel)
+	// time.Sleep(1 * time.Second)
+	fmt.Println("received!")
 }
