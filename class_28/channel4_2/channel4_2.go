3f445cbed3164f3c204f5f5100d6ea537664d7b8
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-23 11:59:13 +0800

    updated

--- class_28/channel4_2/channel4_2.go
+++ class_28/channel4_2/channel4_2.go
@@ -13,6 +13,10 @@ func main() { //this main goroutine is our receiving goroutine
 
 func greetings(myChannel chan string) { //this is our sending goroutine
 	// myChannel <- "Hi"
+	fmt.Println("From greetings goroutine...")
 }
 
 /*A receive operation blocks the receiving goroutine until another goroutine executes a send operation on the same channel. */
+
+/*Q: why we got dead lock error?
+Ans: As we know that A receive operation blocks the receiving goroutine until another goroutine executes a send operation on the same channel. So in this situation our receiving goroutine is our main function, so the receive operation block this main function, when main function is blocked then there's nothing to execute. That's why we got deadlock error.*/
