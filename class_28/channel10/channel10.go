80455b94502de0cf9c0802309cce7e7e5b226b8a
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-23 18:55:59 +0800

    updated

--- class_28/channel10/channel10.go
+++ class_28/channel10/channel10.go
@@ -8,13 +8,12 @@ import (
 	"net/http"
 )
 
-type page struct {
-	url string
-	size int
+type Page struct {
+	URL string
+	Size int
 }
 
-func responseSize(url string, myChannel chan page) {
-	//fmt.Println("Getting",url)
+func responseSize(url string, myChannel chan Page) {
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -24,17 +23,17 @@ func responseSize(url string, myChannel chan page) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	myChannel <- page{url,len(body)}
+	myChannel <- Page{URL: url,Size: len(body)}
 }
 
 func main() {
-	pages := make(chan page)
+	pages := make(chan Page)
 	urls := []string{"https://example.com","https://golang.org/","https://golang.org/doc"}
 	for _, url := range urls {
 		go responseSize(url, pages)
 	}
 	for i := 0; i < len(urls); i++ {
-		page := <-pages
-		fmt.Printf("%s : %d\n",page.url,page.size)
+		Page := <-pages
+		fmt.Printf("%s : %d\n",Page.URL,Page.Size)
 	} 
 }
