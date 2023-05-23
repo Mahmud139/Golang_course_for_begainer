d49eda193905443423237e0d65ab8e623292e552
mahmud139 <mahmudulhasan139139@gmail.com>
2023-05-23 11:50:27 +0800

    updatd

--- class_28/channel4_2/channel4_2.go
+++ class_28/channel4_2/channel4_2.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+func main() { //this main goroutine is our receiving goroutine
+	myChannel := make(chan string)
+	go greetings(myChannel)
+	receiveValue := <- myChannel
+
+	fmt.Println("This line won't print out until receiving goroutine receive the data")
+	fmt.Println(receiveValue)
 }
+
+func greetings(myChannel chan string) { //this is our sending goroutine
+	// myChannel <- "Hi"
+}
+
+/*A receive operation blocks the receiving goroutine until another goroutine executes a send operation on the same channel. */
